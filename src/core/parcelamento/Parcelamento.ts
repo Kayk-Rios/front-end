3725461b3b43994906a29d34b50ed6b51ef7c42a
Kayk-Rios <manjac688@gmail.com>
2024-10-01 12:53:04 -0300

    Fear:AdicionandoArrayProdutos/Parcela/Precificacoes/index

---
+++ src/core/parcelamento/Parcelamento.ts
@@ -0,0 +1,6 @@
+export default interface Parcelamemto{
+    valorTotal: number;
+    valorParcela: number;
+    qtdeParcelas: number;
+    taxaJuros: number;
+}
--- src/core/produto/Precificavel.ts
+++ src/core/produto/Precificavel.ts
@@ -1,7 +1,7 @@
 export default interface Precificavel{
-    precoBase: number
-    precoPromocional:number
-    menoPreco: number;
+    precoBase: number;
+    precoPromocional:number;
+    menorPreco: number;
     maiorPreco: number;
     precoMedio: number;
 }
