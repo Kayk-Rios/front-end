6f77718ceaddf74976381ebe36ed0beb6e344216
Kayk-Rios <manjac688@gmail.com>
2024-10-01 01:48:00 -0300

    feat: adicionando coreEproduto_regrasdenegocio

---
+++ src/core/produto/Produto.ts
@@ -0,0 +1,12 @@
+
+export default interface Produto{
+    id: number;
+    nome: string;
+    descricao: string;
+    marca: string;
+    modelo: string;
+    imagem: string;
+    nota: number;
+    videoReview: string;
+    tags: string[];
+}
